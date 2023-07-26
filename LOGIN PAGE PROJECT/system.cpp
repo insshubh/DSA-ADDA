cd5c7315333c07498f7594f70a34f3d3bdd628ee
Shubham Kumar <insshubh22@gmail.com>
2023-07-26 22:37:34 +0530

    project1

---
+++ LOGIN PAGE PROJECT/system.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <fstream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+    int choice_val;
+
+    const string ANSI_BOLD = "\033[1m";
+    const string ANSI_RESET = "\033[0m";
+    system("cls");
+    // Page Look
+     cout << ANSI_BOLD << "                                        o   o " << ANSI_RESET<<endl;
+     cout << ANSI_BOLD << "                                          |   " << ANSI_RESET<<endl;
+     cout << ANSI_BOLD << "                                         \\_/  " << ANSI_RESET<<endl;
+
+
+    cout << ANSI_BOLD << endl
+         << endl
+         << endl;
+    cout << ANSI_BOLD << "                 LOGIN PAGE                " << ANSI_RESET << endl;
+    cout << ANSI_BOLD << "-------------------------------------------" << ANSI_RESET << endl
+         << endl;
+    cout << ANSI_BOLD << " __________________________________________" << ANSI_RESET << endl;
+    cout << ANSI_BOLD << "|__________________________________________|" << ANSI_RESET << endl;
+    cout << ANSI_BOLD << "|               1. Register                | " << ANSI_RESET << endl;
+    cout << ANSI_BOLD << "|                                          | " << ANSI_RESET << endl;
+    cout << ANSI_BOLD << "|               2. Login                   | " << ANSI_RESET << endl;
+    cout << ANSI_BOLD << "|                                          | " << ANSI_RESET << endl;
+    cout << ANSI_BOLD << "|               3. Testimonial             | " << ANSI_RESET << endl;
+    cout << ANSI_BOLD << "|                                          | " << ANSI_RESET << endl;
+    cout << ANSI_BOLD << "|__________________________________________| " << ANSI_RESET << endl;
+    cout<<endl<<endl;
+    cout << ANSI_BOLD << "Enter Your Choice" << ANSI_RESET<<endl;
+    cin>>choice_val;
+    
+
+
+
+
+    return 0;
+}
